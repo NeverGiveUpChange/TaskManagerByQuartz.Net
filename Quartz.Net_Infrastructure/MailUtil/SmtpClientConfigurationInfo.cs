9065020de54772d6a64f2836693bace823db38f6
NeverGiveUpChange <13220105380@163.com>
2018-07-24 16:22:28 +0800

    修改一些敏感信息

--- Quartz.Net_Infrastructure/MailUtil/SmtpClientConfigurationInfo.cs
+++ Quartz.Net_Infrastructure/MailUtil/SmtpClientConfigurationInfo.cs
@@ -9,9 +9,9 @@ namespace Quartz.Net_Infrastructure.MailUtil
 {
     public static class SmtpClientConfigurationInfo
     {
-        public static string Host { get { return "smtp.163.com"; } private set { } }
-        public static string UserName { get { return "strive_cl@163.com"; } private set { } }
-        public static string PassWord { get { return "1234567abcdefg"; } private set { } }
+        public static string Host { get { return "xxxxx"; } private set { } }
+        public static string UserName { get { return "sxxxxx"; } private set { } }
+        public static string PassWord { get { return "xxxxx"; } private set { } }
         public static SmtpDeliveryMethod SmtpDeliveryMethod { get{ return SmtpDeliveryMethod.Network; }private set { } }
         public static bool   UseDefaultCredentials { get { return true; }private set { } }
     }
